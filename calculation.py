e527be76135b4557a6bc2e92d14e684354cde5f3
Vladimir Danilov <94477929+VladimirDanilov97@users.noreply.github.com>
2022-11-02 10:53:50 +0300

    Update calculation.py

--- calculation.py
+++ calculation.py
@@ -14,28 +14,23 @@ class Cylyndre():
         self.d
 
 class TorosphiricalDish():
-    def __init__(self, D, R, r, h, s, H) -> None:
-        self.D = D
+    def __init__(self, D, R, r, h, s) -> None:
+        self.D = D-2*s
         self.R = R
         self.r = r
-        self.h = h
-        self.s = s
-        self. H = H
+        self.h = h       
     
     def calculate_inner_volume(self):
         k = self.r/self.D # knuckle-radius parameter
         f = self.R/self.D # dish-radius parameter
 
         alpha = asin( (1 - 2*k)/(2*(f-k)) )
-        print(alpha/pi*180)
 
         a_1 = f * self.D * (1 - cos(alpha))
         a_2 = k * self.D * cos(alpha)
         D_1 = (2*f * self.D * sin(alpha))
         s = (k * self.D * sin(alpha))**2
         t = 2*a_2
-
-        print(a_1, a_2, D_1, s, t)
 
         V_1 = pi/4 * (2*a_1**3/3 + a_1*D_1**2/2)
         V_2 = pi*a_2 * ( (self.D/2 - k*self.D)**2 + s )
@@ -44,7 +39,7 @@ class TorosphiricalDish():
         V_4_2 = t*sqrt(s)/4 + k**2*self.D**2/2*asin(cos(alpha)) 
         V_4 = V_4_1 * V_4_2
         V_5 = pi*self.D**2*self.h/4 
-        print(V_1, V_2, V_3, V_4, V_5)
+
         inner_volume = V_1 + V_2 + V_3 + V_4 + V_5
-        return round(inner_volume, 3)       
+        return round(inner_volume, 3)  
         
